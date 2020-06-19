a11f170bd02ef611dcd67b1334d11f05140c98ef
ryanck04 <ryanckendrick4@gmail.com>
2020-06-19 11:54:08 +0000

    update

--- main.cs
+++ main.cs
@@ -9,17 +9,17 @@ class MainClass {
     double salesamount  = Convert.ToDouble(input);
     double sc = 200 +(0.09 *salesamount);
     Console.WriteLine("Sales commission for" + " "+ " "+ name+" "+ "is"+ " "+"$"+sc);
-    if(sc  <= 2999){
+    if(sc>=0 && sc<= 2999){
       Console.WriteLine("Performance is poor");
 
     }
-    else if(sc <= 4999){
+    else if(sc>=3000 && sc <= 4999){
       Console.WriteLine("Performance is average");
     }
-    else if(sc <=9999){
+    else if(sc>=5000 && sc <=9999){
       Console.WriteLine("Performance is good");
     }
-    else if(sc <=14999){
+    else if(sc>=1000 && sc <=14999){
       Console.WriteLine("Performance is excellent");
 
     }
